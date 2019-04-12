7705c7ef36942185ffbaae93d1497ec7c6fe1100
lawlz <jimmyjameslawlz@gmail.com>
2019-04-12 15:41:03 -0500

    Initial Commit
    This has things that need work, but hello world

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,241 @@
+﻿<#
+      .Synopsis
+          This is my local PS profile settings.
+      .DESCRIPTION
+          This is a starter profile or helper profile.
+          
+          There are some alias's that are created.
+          It checks if you are admin or not makes the words red if admin or green if not.
+          It adds some paths to your global script/cmdlet search path - easier to run commands like tshark and others.
+
+
+      .Dependencies
+          Powershell 5+
+      
+      Resources:
+
+
+      .AUTHOR
+        James Lawler
+        DATE:11/20/2014
+      
+      .EXAMPLE
+      
+      . $env:UserProfile\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1
+
+      Dot sourcing the profile, but if you have it in this locatino it should source by default. Maybe you changed it?
+
+#>
+
+#For this first part I am pretty much just following this page:
+#http://www.howtogeek.com/50236/customizing-your-powershell-profile/
+
+#before you profile do this:
+# Test-Path $profile
+
+#If nothing found then do this command:
+# if (test-path $profile) {
+#    New-Item -path $profile -type file –force
+# }
+#you should see a popup, just say OK
+
+#alias's
+set-alias ll get-childitem
+set-alias wc measure-object
+set-alias ifconfig ipconfig
+# way to set an alias via function <-- not really alias then...
+function md5sum {get-filehash -algorithm md5}
+# do a man on them and see, one shows the alias's commands help
+# the other shows nothing and expects those help attribs to be in the function.
+
+# change up the terminal window
+# I had some help on this one. 
+# https://gallery.technet.microsoft.com/scriptcenter/Set-the-PowerShell-Console-bd8b2ad1
+if ($host) {
+  # here i attempted to modify the current shell to have some defaults that I prefer.
+  # I tried to base it off the current screen size to position to and all that.  Epic Fail...
+  # $shell = $host.ui.RawUI
+  # # now get the shell size
+  # $maxHeight = $shell.MaxPhysicalWindowSize.Height
+  # $maxWidth = $shell.MaxPhysicalWindowSize.Width
+  # # now get the buffer settings, we want lotso scroll
+  # $myBuffer = $shell.buffersize
+  # # now getting the active windowsize interface to modify
+  # $myWindow = $shell.Windowsize
+  # # modify stuffs
+  # $MyWindow.Height = ($MaxHeight)
+  # $MyWindow.Width = ($Maxwidth-2)
+  # $MyBuffer.Height = (9999)
+  # $MyBuffer.Width = ($Maxwidth-2)
+  # $shell.set_bufferSize($MyBuffer)
+  # $shell.set_windowSize($MyWindow)
+  #there is this weird initial positioning that keeps screwing this up. 
+  # .net option to tickle: System.Management.Automation.Host.Coordinates  -- nope...
+  # You can use the process namespace, first get current pid - Already Done!  $pid var...
+  # Determined that I don't really care.  I can hotkey to the position I want...
+
+  # now check if you are admin or not, so you know not to screw around.
+  #http://superuser.com/questions/237902/how-can-one-show-the-current-directory-in-powershell
+  $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+  if ($isAdmin) {
+    # modifying the prompt variable, one for admin and the other for reg user.
+    function prompt {
+      # we want to see if the last result ($?) ran successfully
+      $(if ($?) {
+        write-host ":) " -ForegroundColor "Green" -NoNewline
+      }
+      else {
+        write-host ":( " -ForegroundColor "Red" -NoNewline
+      }) + $(write-host "$(get-location)" -foregroundcolor "darkred" -NoNewline) +
+      ">"
+    }
+    # set admin window settings
+    # custom mods of the terminal shell:
+    $Shell = $Host.UI.RawUI
+    $size = $Shell.BufferSize
+    $size.width=240
+    $size.height=9999
+    $shell.BackgroundColor = "Black"
+    $shell.ForegroundColor = "DarkRed"
+    $shell.windowtitle = "You are PowerShell'n as an Administrator!"
+  }
+  else {
+    # regular user prompt modify
+    function prompt {
+      # we want to see if the last result ($?) ran successfully
+      # based on that result color the face and make frown or smile.
+      $(if ($?) {
+        write-host ":) " -ForegroundColor "Green" -NoNewline
+      }
+      else {
+        write-host ":( " -ForegroundColor "Red" -NoNewline
+      }) + $(write-host "$(get-location)" -foregroundcolor "green" -NoNewline) +
+      ">"
+    }
+    # custom mods of the shell:
+    $Shell = $Host.UI.RawUI
+    $size = $Shell.BufferSize
+    $size.width=240
+    $size.height=9999
+    $shell.BackgroundColor = "Black"
+    $shell.ForegroundColor = "Green"
+    $shell.windowtitle = "You are PowerShell'n as a normal user."
+  }
+}
+else 
+{
+  write-host "For some reason you don't have the $host var set."
+  $shell = $host
+  if ($shell){
+    write-host "Yay you got the host var, now do while?"
+  }
+}
+#since moving around a profile, may not have all the paths there in the new env
+# we don't want to add paths for tab completion when not valid, this attempts that
+function check-path{
+  param(
+    [parameter(mandatory=$true,position=0,valuefrompipelinebypropertyname=$true)]$path
+  )
+  if(test-path $path){
+    return $path
+  }
+}
+
+# create dynamic entry point, based on current env
+$userRootDir = $env:USERPROFILE
+# create array to add valid paths to
+$paths = @()
+# this may not be the best way to add a path
+# just paste the valid path string in one of these, and it should do the rest.
+$paths += check-path "$userRootDir\Documents\repo\tools\android-tools"
+$paths += check-path "C:\Program Files (x86)\Nmap"
+$paths += check-path "C:\Program Files\Git\bin"
+$paths += check-path "$userRootDir\Documents\repo\scripts\powershell"
+$paths += check-path "$userRootDir\Documents\repo\Tools\SysinternalsSuite"
+$paths += check-path "$userRootDir\Documents\GitHub\DidierStevensSuite"
+$paths += check-path "C:\Program Files\nodejs"
+$paths += check-path "C:\Program Files\Python37"
+$paths += check-path "$userRootDir\Documents\repo\scripts\python"
+# now to create a string var to append to
+[string]$allPaths = ""
+foreach ($location in $paths) {
+  # we have to break up each path with a semicolon
+  $allPaths = $allpaths + ";" + $location
+}
+# now add those valid paths to env path
+$env:Path = $env:Path + $allPaths
+
+#good ol' . ("dot") sourcing in powershell
+# if you have a custom functions you want to load, like internal corp functions, use this.
+if (test-path "$userRootDir\Documents\repo\scripts\powershell\functions.ps1") {
+  . $userRootDir\Documents\repo\scripts\powershell\functions.ps1
+}
+
+#variables, to make it easier to cd into
+$pygit = "$userRootDir\Documents\repo\git\projects\Scripts\Python"
+$pymods = "$userRootDir\Documents\analyst\tools\DidierStevensSuite"
+
+# by default $home is the root of your profile, who stores stuff there
+# you can still cd $home and get to the root of profile..., but I want docs.
+$goToNewHome = $userRootDir + "\Documents"
+set-location $goToNewHome
+# Set the "~" shortcut value for the FileSystem provider
+# it doesn't go to the root of your profile like *nix, but docs, which is where I want to be usually in PowerShell
+(get-psprovider 'FileSystem').Home = $goToNewHome
+
+
+
+Function Global:draw-figure
+{
+Write-Host -ForegroundColor green @"                                            
+                                            ';'                  
+                                          '+;:;'#@+.             
+                                         @;';::::::@@`           
+                                        @:;::;:::::;;@`          
+                                       .#::::;::::::::@          
+                                       @::::::::::::::;@ `       
+                                      ,+;;::::::::::::;''        
+                                      #:::::::::::::::::@        
+                                      @;::::::::::::::::;;       
+                                     `;::::::::::::::::::@       
+                                     +:;::;;:::::;:::::::@.      
+                                     @:#@++#';:::;'@;:@;:'@      
+                      `              @@......::::;+:....+:;@      
+        :+;+.       ,  :             @........@::;'.......':@      
+        +  ,+       .. ,             #++......@::',......`@:@      
+        :   #      .. `,             #,;......@;:+.#......@:@      
+        :   +      ;..`              #.......`';:+;#.....;@:@      
+        +.:;+     .,#@               @#......`@;:;@......@@:@      
+        +,.;+     ,`+`               @@:....+@:::@@+...:@@':@      
+    `   +...+    ;`...               @;@@;#@@+:::#@@@@@@:;@#     
+  @@@@, +:;,+    .,..                +'#@@@@#';:::@@@@+::+@##:   
+ ;@++@@ `...    ,`,`,                `@:;;;:'::::;'::';::@####.  
+ :@#++@ `..,     ,..                  @;:::::::::::::;:::@#####  
+  #++#@ .,..    .:..                  ;@:::::;::;;:::::::@#####` 
+  :#@@` @;;;.   ::`                    @:;:::;:::::;::;:@######' 
+   ;#@,  ''     #:`                    #'::+'#';;+'':;::@####### 
+   ,;''  ;;     #:`                     @+:@@+:.,;@++::@######## 
+   `;;;  `:'    +::                     ,#'#,;@@@@+,:;@@######## 
+    ';;   ;:    +:+                   `  :@+;#+:@,';#@@######### 
+    ''''  :;'   ;:;                        `#@#+++@@############ 
+    #';'  `::+  `::'                          ;################# 
+    ,''+   ;:;#  ;:'                          ;################# 
+     `;::   +:;; #:;                          ;################# 
+   `   :::`  ;::+.:;'                         ;################# 
+       .::'`  #;:;+;:`                        #+################ 
+      ` `:;:+# @;+;:###'`                    ,+################+ 
+         `;:::;:'+;;#+####'                  +#################+ 
+           ;:;;;;:::#+######'               :##################; 
+             ,+;:;:;:#######+##,            ###################: 
+                ....  .####+#####'        `+###################, 
+                        `#######+###.     `####################, 
+                          .#++++#+#++#'   +#+#++++++++++++++++#. 
+                             ;+''++++++++.+++++++++++++++++++++` 
+                               :'';'''''';';;;;''''''''''''''';` 
+                                 `:::::::::::,:::::::::::::::::  
+                                    `......`. .................
+"@
+} 
+
+Set-Alias wat draw-figure -Scope Global
+
---
+++ README.md
@@ -0,0 +1,140 @@
+# Overview
+
+This is the working directory of the presentation 'Powershell to the People'
+
+
+## Outline
+
+* Basics I wish I knew early on
+    * Verb-noun wat?!
+    * Help Get-Help
+    * Get-member
+    * format outputs
+    * what is $_,?,%, $?, etc
+* PowerShell Profiles
+    * walk through the profile I have 
+* Never open ADUC again!
+    * How many active users
+    * How many passwords never set on enabled users
+    * How many people in a group
+        * compare some groups
+* Tickle Outlook to send mail
+* Scheduled Task?
+    * add-type to do this?  
+        Oh yeah do that!
+
+
+
+
+## Rundown
+
+1. Basics
+    * cmdlet naming convention
+        * Does seem bloated at first, but can become more natural feeling  
+        * verb-noun.ps1 construct
+            * approved verb list (get-verb - seriously will list approved verbs...)
+            * You don't have to conform - use case
+                * All the internal PS scripts for my internal compX usages starts like this:
+                    `CompXGet-Computer.ps1`
+                * That way tab completion worked for internal commands, your IDE will get mad..
+    * get-help - man for Windows
+        * man seems to be a built in alias now for get-help...
+        * search for name of cmdlet, function, script, or workflow in the first parameter 
+            * If you explicity state it, it goes to it
+            * If not, it searches help topic titles
+                * if not found then show topics that include that word
+    * get-member - exploring objects
+        * I use this extensively to see what methods are available for an object
+            * What is the type of the object
+    * format output options
+        * the many ways, from csv, xml, windows form type, list, table, etc
+            * convertto-csv, convertto-xml, out-gridview, format-list, format-table, out-host
+
+2. PowerShell Profiles
+    * Like in Bash, but PS has it too!
+        * some configuration required - depedning on how you are accessing PS
+            * VSCode has a path
+            * ISE has a path
+            * PS Terminal has a path..
+        * Luckily there is a common default path, for current user:
+            `$home\Documents\WindowsPowerShell\profile.ps1`  
+        **Must Change Execution Policy to Work** 
+    * Lets go through mine
+        * Found some issues with coloring the command line... 
+            * It has something to do with PSReadLine... See here:   
+                [1]: https://github.com/MicrosoftDocs/PowerShell-Docs/issues/2688  
+                [2]: https://github.com/lzybkr/PSReadLine/issues/818  
+                [3]: https://github.com/lzybkr/PSReadLine/issues/774  
+                [4]: https://github.com/Microsoft/console/issues/276  
+                * doing a remove module fixes my colorizations, and I still get features of this module somehow...  
+                `remove-module psreadline`  
+                * They say however, PSReadline is the new way of setting colours is better and the way forward... I guess I will conform.  
+        * Set Alias's you like  
+
+
+3. PS AD Tools
+    * Why use a GUI when all the power of ADUC is in PS ADTools
+        * It scales and is much quicker to get results.
+    * find the functional level of the domain with one commandlet!  
+        * `get-adrootDSE` or like this `(Get-ADRootDSE).domainFunctionality`
+    * Who has a password to never expire in your domain?  
+        * `get-aduser -Filter "PasswordNeverExpires -eq 'True'"`
+    * Find out who are all members of Domain Admins Group
+
+4. Send some mails
+    * Situation, had to update .msg file to send a phishing removel/malware endpoint events
+        * too cumbersome and the perfect opp to flex some powershell skills
+    * send-mailMessage to the rescue!
+        * sadly no adoption because could not validate the email template
+        * even dumping the message didn't help
+    * enter, tickling the Outlook API!!
+        * Pops open an outlook message with all the stuffs
+        * more easily adopted and accepted by others in IR
+
+5. Add Yo Type Right Here
+    * Are you so attached to your C# you can't give it up?
+
+
+
+## Code
+
+
+
+
+
+## References
+
+
+
+
+Automatic variables to call - [good ol' built-ins](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables?view=powershell-6)  
+More information about the [powershell profile](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_prompts?view=powershell-5.1  )
+
+Weird command cannot be found then hit enter again and sure enough, [its there..](https://social.msdn.microsoft.com/Forums/en-US/2c40c928-ce5e-460d-a1ef-30c5ef494846/why-does-it-say-command-cannot-be-found?forum=WindowsIoT)  
+I believe the problem is, PS is searching the client machine and not the remote session.  Which is definitely the case, installed activedirectory module on local machine and it fixed that weird error.    
+
+Great resource on converting C/C++ types (generally the way MS shows you in their dev docs) to [.net types powershell can use](http://www.pinvoke.net/)  
+
+Great profile information with additional PS Profile links at the bottom of [page](https://blogs.technet.microsoft.com/askpfeplat/2018/06/25/powershell-profiles-processing-illustrated/)  
+
+WinRM can be difficult to setup.  Had to enable the [winrm quickconfig](https://4sysops.com/wiki/enable-powershell-remoting/)  
+This link help create the [HTTPS listener](https://www.visualstudiogeeks.com/devops/how-to-configure-winrm-for-https-manually)  
+Once you configure the cert, you can run the export-certificate cmdlet like so:  
+    `$cert = gci Cert:\LocalMachine\My\CertThumbprint`  
+    `Export-Certificate -Cert $cert -FilePath "C:\Users\Administrator\Documents\newCert" -Type cert`  
+I then mounted the C$ share and copied down the cert that I exported above.  
+Finally the fix, run PS with *elevated* creds from client [side Powershell](https://serverfault.com/questions/337905/enabling-powershell-remoting-access-is-denied/568228#568228)  
+I was able to remote from a non-domain joined machine.  
+
+
+BlueTeam Resources:  
+[Some common commands to know](https://github.com/sans-blue-team/blue-team-wiki/blob/gh-pages/Tools/PowerShell.md)  
+[Nice little repo of scripts.](https://github.com/WiredPulse/PowerShell)
+
+
+Install RSAT, now a 'Feature On Demand' but the GUI sucks and never really installs..
+[I had to use DISM to do it](https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/dism-capabilities-package-servicing-command-line-options)  
+First get the list of capabilities online.  
+`dism /online /get-capabilities`  
+Then copy the name and add it to the capabilityName param  
+`dism /add-capability /online /CapabilitYName:"Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"`
